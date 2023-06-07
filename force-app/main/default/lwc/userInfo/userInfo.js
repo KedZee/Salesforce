8a557672ea367d06124821fb4a0e53dba92928be
S.Klakotskij <s.klakotski@techin.by>
2023-06-07 22:53:14 +0300

    bring back import { NavigationMixin }

--- force-app/main/default/lwc/userInfo/userInfo.js
+++ force-app/main/default/lwc/userInfo/userInfo.js
@@ -1,3 +1,5 @@
+import { NavigationMixin } from 'lightning/navigation';
+
 const userInfo = (superclass) => class extends NavigationMixin(superclass) {
     showConsoleLog() {
         console.log('Hello User');
