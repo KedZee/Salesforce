f157ba0d2dc6ab06a9e31c3758366b2dccac3a15
S.Klakotskij <s.klakotski@techin.by>
2023-06-09 02:39:09 +0300

    complited first task

--- force-app/main/default/lwc/testComponent/testComponent.js
+++ force-app/main/default/lwc/testComponent/testComponent.js
@@ -1,7 +1,8 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
 import { UserInfo } from 'c/userInfo';
 
 export default class TestComponent extends UserInfo(LightningElement) {
+
 
     connectedCallback() {
         this.showConsoleLog();
