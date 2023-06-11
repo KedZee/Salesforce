e8dea3f1f209b19b1930e0c20987548e81e31b01
S.Klakotskij <s.klakotski@techin.by>
2023-06-11 22:37:19 +0300

    a chain of promises has been created

---
+++ force-app/main/default/lwc/testPromise/testPromise.js
@@ -0,0 +1,82 @@
+import { LightningElement } from 'lwc';
+import firstMultiplyBy2 from '@salesforce/apex/mockDelays.firstMultiplyBy2';
+import secondMultiplyBy2 from '@salesforce/apex/mockDelays.secondMultiplyBy2';
+import thirdMultiplyBy2 from '@salesforce/apex/mockDelays.thirdMultiplyBy2';
+import fourthMultiplyBy2 from '@salesforce/apex/mockDelays.fourthMultiplyBy2';
+import fifthMultiplyBy2 from '@salesforce/apex/mockDelays.fifthMultiplyBy2';
+
+export default class TestPromise extends LightningElement {
+
+    // это первый вариант, который мог бы быть, но от него я отказался, потому что
+    // код растет в ширь
+
+    // connectedCallback() {
+    //     firstMultiplyBy2({inputNumber: 1}).then(result => {
+    //         console.log('result=1==', result)
+    //         secondMultiplyBy2({inputNumber: 2}).then(result => {
+    //             console.log('result=2==', result)
+    //             thirdMultiplyBy2({inputNumber: 3}).then(result => {
+    //                 console.log('result=3==', result)
+    //                 fourthMultiplyBy2({inputNumber: 4}).then(result => {
+    //                     console.log('result=4==', result)
+    //                     fifthMultiplyBy2({inputNumber: 5}).then(result => {
+    //                         console.log('result=5==', result)
+    //                     })
+    //                 })
+    //             })
+    //         })
+    //     })
+    // }
+
+    firstResult;
+    secondtResult;
+    thirdResult;
+    fourthResult;
+    fifthResult;
+
+    connectedCallback() {
+        this.callFirstPromise()
+    }
+
+    callFirstPromise() {
+        firstMultiplyBy2({inputNumber: 1})
+            .then(result => {
+                this.firstResult = result;
+                this.callSecondPromise();
+            })
+    }
+
+    callSecondPromise() {
+        secondMultiplyBy2({inputNumber: 2})
+            .then(result => {
+                this.secondtResult = result;
+                this.callThirdPromise();
+            })
+    }
+
+    callThirdPromise() {
+        thirdMultiplyBy2({inputNumber: 3})
+            .then(result => {
+                this.thirdResult = result;
+                this.callFourthPromise();
+            })
+    }
+
+    callFourthPromise() {
+        fourthMultiplyBy2({inputNumber: 4})
+            .then(result => {
+                this.fourthResult = result;
+                this.callFifthPromise();
+            })
+    }
+
+    callFifthPromise() {
+        fifthMultiplyBy2({inputNumber: 5})
+            .then(result => {
+                this.fifthResult = result;
+            })
+    }
+
+
+
+}
